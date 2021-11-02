4b844855d73afedaca5069384cfaf574fa217a16
Pablo Henrique <pablo_hen@hotmail.com>
2021-11-02 15:11:04 -0300

    .

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Toaster } from 'react-hot-toast';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { BrowserRouter } from 'react-router-dom';
+import Contexts from './contexts';
+import Routes from './routes';
+import GlobalStyle from './styles/globals';
 
 function App() {
+  const queryClient = new QueryClient();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <QueryClientProvider client={queryClient}>
+        <Contexts>
+          <Routes />
+          <GlobalStyle />
+          <Toaster position="bottom-center" />
+        </Contexts>
+      </QueryClientProvider>
+    </BrowserRouter>
   );
 }
 
