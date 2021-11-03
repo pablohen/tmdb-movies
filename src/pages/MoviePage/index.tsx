1660a025f3cc6b6d04e0b992e318e6b8e5a174e7
Pablo Henrique <pablo_hen@hotmail.com>
2021-11-03 08:57:01 -0300

    add: FadeIn

--- src/pages/MoviePage/index.tsx
+++ src/pages/MoviePage/index.tsx
@@ -1,4 +1,5 @@
 import React, { useEffect } from 'react';
+import FadeIn from 'react-fade-in';
 import { useParams } from 'react-router-dom';
 import GenreButton from '../../components/GenreButton';
 import Header from '../../components/Header';
@@ -46,104 +47,106 @@ const MoviePage = (props: Props) => {
         {isLoadingMovie && <Loading />}
 
         {!isLoadingMovie && !!movie.title && (
-          <Content>
-            <Title>
-              <h2>{movie.title}</h2>
-              <span>{new Date(movie.release_date).toLocaleDateString()}</span>
-            </Title>
-            <Body>
-              <Details>
-                {!!movie.overview && (
-                  <>
-                    <h3>Sinopse</h3>
+          <FadeIn>
+            <Content>
+              <Title>
+                <h2>{movie.title}</h2>
+                <span>{new Date(movie.release_date).toLocaleDateString()}</span>
+              </Title>
+              <Body>
+                <Details>
+                  {!!movie.overview && (
+                    <>
+                      <h3>Sinopse</h3>
 
-                    <p>{movie.overview}</p>
-                  </>
+                      <p>{movie.overview}</p>
+                    </>
+                  )}
+
+                  <h3>Informações</h3>
+
+                  <div className="info">
+                    <div>
+                      <h4>Situação</h4>
+                      <p>{movie.status}</p>
+                    </div>
+                    <div>
+                      <h4>Idioma</h4>
+                      <p>{movie.original_language}</p>
+                    </div>
+                    <div>
+                      <h4>Duração</h4>
+                      <p>{calcRuntime(movie.runtime)}</p>
+                    </div>
+                    <div>
+                      <h4>Orçamento</h4>
+                      <p>
+                        {new Intl.NumberFormat('pt-BR', {
+                          style: 'currency',
+                          currency: 'USD',
+                        }).format(movie.budget)}
+                      </p>
+                    </div>
+                    <div>
+                      <h4>Receita</h4>
+                      <p>
+                        {new Intl.NumberFormat('pt-BR', {
+                          style: 'currency',
+                          currency: 'USD',
+                        }).format(movie.revenue)}
+                      </p>
+                    </div>
+                    <div>
+                      <h4>Lucro</h4>
+                      <p>
+                        {new Intl.NumberFormat('pt-BR', {
+                          style: 'currency',
+                          currency: 'USD',
+                        }).format(movie.revenue - movie.budget)}
+                      </p>
+                    </div>
+                  </div>
+
+                  <div className="genres">
+                    {movie.genres &&
+                      movie.genres.map((genre) => (
+                        <GenreButton key={genre.id} text={genre.name} />
+                      ))}
+                  </div>
+
+                  <div className="rating">
+                    <span>{movie.vote_average * 10}%</span>
+                  </div>
+                </Details>
+
+                {!!movie.poster_path && (
+                  <Image>
+                    <img
+                      src={`https://image.tmdb.org/t/p/original/${movie.poster_path}`}
+                      alt={movie.title}
+                      width="360px"
+                      height="auto"
+                    />
+                  </Image>
                 )}
+              </Body>
 
-                <h3>Informações</h3>
-
-                <div className="info">
-                  <div>
-                    <h4>Situação</h4>
-                    <p>{movie.status}</p>
-                  </div>
-                  <div>
-                    <h4>Idioma</h4>
-                    <p>{movie.original_language}</p>
-                  </div>
-                  <div>
-                    <h4>Duração</h4>
-                    <p>{calcRuntime(movie.runtime)}</p>
-                  </div>
-                  <div>
-                    <h4>Orçamento</h4>
-                    <p>
-                      {new Intl.NumberFormat('pt-BR', {
-                        style: 'currency',
-                        currency: 'USD',
-                      }).format(movie.budget)}
-                    </p>
-                  </div>
-                  <div>
-                    <h4>Receita</h4>
-                    <p>
-                      {new Intl.NumberFormat('pt-BR', {
-                        style: 'currency',
-                        currency: 'USD',
-                      }).format(movie.revenue)}
-                    </p>
-                  </div>
-                  <div>
-                    <h4>Lucro</h4>
-                    <p>
-                      {new Intl.NumberFormat('pt-BR', {
-                        style: 'currency',
-                        currency: 'USD',
-                      }).format(movie.revenue - movie.budget)}
-                    </p>
-                  </div>
-                </div>
-
-                <div className="genres">
-                  {movie.genres &&
-                    movie.genres.map((genre) => (
-                      <GenreButton key={genre.id} text={genre.name} />
-                    ))}
-                </div>
-
-                <div className="rating">
-                  <span>{movie.vote_average * 10}%</span>
-                </div>
-              </Details>
-
-              {!!movie.poster_path && (
-                <Image>
-                  <img
-                    src={`https://image.tmdb.org/t/p/original/${movie.poster_path}`}
-                    alt={movie.title}
-                    width="360px"
-                    height="auto"
-                  />
-                </Image>
-              )}
-            </Body>
-
-            <div className="trailers">
-              {!isLoadingTrailers &&
-                trailers.map((trailer) => {
-                  if (trailer.site === 'YouTube') {
-                    return (
-                      <MovieTrailer
-                        key={trailer.id}
-                        youtubeVideoKey={trailer.key}
-                      />
-                    );
-                  }
-                  return null;
-                })}
-            </div>
-          </Content>
+              <div className="trailers">
+                {!isLoadingTrailers &&
+                  trailers.map((trailer) => {
+                    if (trailer.site === 'YouTube') {
+                      return (
+                        <MovieTrailer
+                          key={trailer.id}
+                          youtubeVideoKey={trailer.key}
+                        />
+                      );
+                    }
+                    return null;
+                  })}
+              </div>
+            </Content>
+          </FadeIn>
         )}
       </Container>
     </Page>
